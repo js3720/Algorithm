aadc85d01b4e69843ab5e481d501cb4b81a84fa9
js3720 <dbstnrla1124@gmail.com>
2022-10-13 21:50:56 +0900

    ãã알고리즘 연습 시작

--- src/ingang/step01/No_01.java
+++ src/ingang/step01/No_01.java
@@ -2,6 +2,6 @@ package ingang.step01;
 
 public class No_01 {
     public static void main(String[] args) {
-        System.out.println("ddddd");
+        System.out.println("yyyyyyy");
     }
 }
--- src/programmers/level0/No_05.java
+++ src/programmers/level0/No_05.java
@@ -1,4 +1,11 @@
 package programmers.level0;
 
 public class No_05 {
+    public static void main(String[] args) {
+        No_05 j = new No_05();
+        System.out.println(j.solution(3,2));
+    }
+    public int solution(int num1, int num2) {
+        return num1%num2;
+    }
 }
