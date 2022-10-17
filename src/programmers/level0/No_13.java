49245393563705181ac50087bb315c03d7a4d1a8
js3720 <dbstnrla1124@gmail.com>
2022-10-17 23:59:48 +0900

    3일차

---
+++ src/programmers/level0/No_13.java
@@ -0,0 +1,12 @@
+package programmers.level0;
+//https://school.programmers.co.kr/learn/courses/30/lessons/120830
+//양꼬치 문제
+public class No_13 {
+    public static void main(String[] args) {
+        No_13 j = new No_13();
+    }
+    public int solution(int n, int k) {
+        int answer = 0;
+        return answer;
+    }
+}
