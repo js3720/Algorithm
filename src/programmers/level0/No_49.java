563ccc2dde1e21b23568f4c71012611e6ccf7d51
js3720 <dbstnrla1124@gmail.com>
2022-11-02 23:59:50 +0900

    18일차

---
+++ src/programmers/level0/No_49.java
@@ -0,0 +1,11 @@
+package programmers.level0;
+import java.util.Scanner;
+//https://school.programmers.co.kr/learn/courses/30/lessons/120823
+//직각삼각형 출력하기
+public class No_49 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("*");
+    }
+}
