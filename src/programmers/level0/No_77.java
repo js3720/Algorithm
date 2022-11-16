1007a7e509c7df328d5227066fe68f0d461cdcec
js3720 <dbstnrla1124@gmail.com>
2022-11-16 23:51:51 +0900

    28일차

--- src/programmers/level0/No_77.java
+++ src/programmers/level0/No_77.java
@@ -5,26 +5,35 @@ import java.util.*;
 public class No_77 {
     public static void main(String[] args) {
         No_77 j = new No_77();
-        System.out.println(j.solution("35+55"));
+        String my_string = "33+55-32";
+        System.out.println(j.solution(my_string));
     }
     public int solution(String my_string) {
         String tmp = "";
+        int answer =0;
         List <Integer> intList = new ArrayList<>();
         List <Character> charList = new ArrayList<>();
         for(int i=0; i<my_string.length(); i++){
-            if(my_string.charAt(i) >='0' || my_string.charAt(i) <='9'){
+            if(my_string.charAt(i) >='0' && my_string.charAt(i) <='9'){
                 tmp+=Character.toString(my_string.charAt(i));
                 if(i==my_string.length()-1) intList.add(Integer.parseInt(tmp));
             }
-            else if(!(my_string.charAt(i) >='0' || my_string.charAt(i) <='9')){
+            else if(my_string.charAt(i) != ' ') {
                 intList.add(Integer.parseInt(tmp));
                 tmp = "";
                 charList.add(my_string.charAt(i));
             }
         }
-        System.out.println(intList.get(0));
-        System.out.println(intList.get(1));
-        System.out.println(charList.get(0));
-        return 0; // 아직 풀지 못한 문제 입니다
+        for(int i=0, j=0; j<charList.size(); j++) {
+            if(j==0){
+                if(charList.get(j)=='+') answer += intList.get(i++)+intList.get(i++);
+                else answer += intList.get(i++)-intList.get(i++);
+            }
+            else{
+                if(charList.get(j)=='+') answer += intList.get(i++);
+                else answer -= intList.get(i++);
+            }
+        }
+        return answer;
     }
 }
