13347198e9f8fa90bae95e5d5a88282ad7fb1b7a
js3720 <dbstnrla1124@gmail.com>
2022-11-21 23:21:58 +0900

    31일차

---
+++ src/programmers/level0/No_87.java
@@ -0,0 +1,19 @@
+package programmers.level0;
+import  java.util.*;
+//https://school.programmers.co.kr/learn/courses/30/lessons/120883
+//로그인 성공?
+public class No_87 {
+    public static void main(String[] args) {
+        No_87 j = new No_87();
+        String[] id_pw = {"meosseug", "123"};
+        String[][] db = {{"rardss", "123"}, {"yyoom", "1234"}, {"meosseugi", "1234"}};
+        System.out.println(j.solution(id_pw,db));
+    }
+    public String solution(String[] id_pw, String[][] db) {
+        String answer ="";
+        for(int i=0; i<db.length; i++){
+            answer = db[i][0].equals(id_pw[0]) ? db[i][1].equals(id_pw[1]) ? "login" : "wrong pw" : "fail";
+        }
+        return answer;
+    }
+}
